f67c9cf5027b14bd36394489e2f39dff68c1cfff
rajapandi29 <rp6321613@gmail.com>
2026-09-10 18:56:56 +0530

    new

--- mini_knowledge/backend.tf
+++ mini_knowledge/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "terraform-state-04-09"
-    key    = "rag-app/terraform.tfstate"
+    key    = "/budgets/terraform.tfstate"
     region = "us-east-1"
     use_lockfile = true
     encrypt = true
